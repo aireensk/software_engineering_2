9aead10499f0fa92af8b1ffa95ddfd24d1dcf2ef
aireensk <aireensk@icloud.com>
2026-04-05 15:11:03 +0000

    updated

--- enterprise-dashboard/backend/blog/tests.py
+++
@@ -1,8 +0,0 @@
-
-from django.test import TestCase
-from django.contrib.auth.models import User
-
-class BasicTest(TestCase):
-    def test_user_creation(self):
-        user = User.objects.create(username="test")
-        self.assertEqual(user.username, "test")
